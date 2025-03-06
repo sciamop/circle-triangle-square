d29b1ca7d004c1d7a3747fdbd49679493cbbb5a1
Todd Faulls <sciamop@hotmail.com>
2025-03-06 18:01:38 -0500

    ugh include new files

---
+++ Scenes/playerCollider.gd
@@ -0,0 +1,14 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#connect("body_entered", _on_body_entered)
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+	#
+#func _on_body_entered(body):
+	#print("Collided with:", body.name)
