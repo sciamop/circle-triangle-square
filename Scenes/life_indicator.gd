f216ddeaf9d1ea3ba1114db9fd6a7cbcb6dd5df7
Todd Faulls <sciamop@hotmail.com>
2025-03-21 22:12:38 -0400

    added new files for projectiles

---
+++ Scenes/life_indicator.gd
@@ -0,0 +1,33 @@
+extends HBoxContainer
+
+@onready var player: Player = $"/root/Game/Player"
+@onready var life_v_box_container_0: VBoxContainer = $lifeVBoxContainer0
+@onready var life_v_box_container_1: VBoxContainer = $lifeVBoxContainer1
+@onready var life_v_box_container_2: VBoxContainer = $lifeVBoxContainer2
+@onready var life_v_box_container_3: VBoxContainer = $lifeVBoxContainer3
+@onready var animation_player: AnimationPlayer = $"../../../../AnimationPlayer"
+
+
+var circleIcon3:Array[Node]
+var circleIcon2:Array[Node]
+var circleIcon1:Array[Node]
+
+func _ready() -> void:
+	var callable = Callable(self, "updateHealth") 
+	player.connect("health_changed", callable)
+	circleIcon3 = life_v_box_container_3.find_children("*","Polygon2D")
+	circleIcon2 = life_v_box_container_2.find_children("*","Polygon2D")
+	circleIcon1 = life_v_box_container_1.find_children("*","Polygon2D")
+
+func updateHealth(current_health:int) -> void:
+	if (current_health == 30):
+		for poop in circleIcon3:
+			poop.color = Color(0.5,0.5,0.5,0.5)
+			animation_player.play("take_damage_3")
+	if (current_health <= 20):
+		for poop in circleIcon2:
+			poop.color = Color(0.5,0.5,0.5,0.5)
+	if (current_health <= 10):
+		for poop in circleIcon1:
+			poop.color = Color(0.5,0.5,0.5,0.5)
+	print(str(current_health))
---
+++ Scripts/projectile.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var speed: float = 600.0
+var direction: Vector2 = Vector2.RIGHT
+var damage: int = 1
+var shape: String = "triangle"
+
+func _ready():
+	# Connect signal for when projectile hits something
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	var active_projectile_shape = find_child("projectile_" + shape)
+	active_projectile_shape.visible = true
+
+func _physics_process(delta):
+	# Move the projectile
+	position += direction.normalized() * speed * delta
+	
+	# Optional: Rotate the projectile to face its movement direction
+	rotation = direction.angle()
+
+func _on_body_entered(body):
+	# Check what was hit
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	
+	# Destroy the projectile
+	queue_free()
+
+# Optional method to free the projectile when it goes off-screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
